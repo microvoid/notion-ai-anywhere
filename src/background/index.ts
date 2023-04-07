ec394a14e97e92ffe9f7bb126f33702063139fc1
smackgg <smackgg@gmail.com>
2023-04-07 16:29:34 +0800

    feat: remove useless code

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,12 +2,6 @@ import { sendToContentScript } from "@plasmohq/messaging"
 
 // Register a keyboard shortcut
 chrome.commands.onCommand.addListener(function (command) {
-  // console.log("start-capture backend", command)
-  if (command === "start-capture") {
-    sendToContentScript({
-      name: "start-capture"
-    })
-  }
   if (command === "show-panel") {
     sendToContentScript({
       name: "show-panel"
